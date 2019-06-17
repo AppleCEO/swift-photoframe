5b42741bd3f410965046a068a9666a6ae21911aa
kiljh <kiljh@me.com>
2019-06-17 14:46:47 +0900

    생성 - GreenViewController, YellowViewController 추가

---
+++ PhotoFrame/PhotoFrame/GreenViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GreenViewController.swift
+//  PhotoFrame
+//
+//  Created by joon-ho kil on 6/17/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#file, #line, #function, #column)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
